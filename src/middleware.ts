b6f25ff46d3c8eaeabd7d73afaface2ce9708bd4
toreyjames <toreyjameshall@gmail.com>
2026-03-09 23:05:50 -0500

    Fix route protection with src middleware matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -21,5 +21,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!.*\\..*).*)"],
+  matcher: ["/", "/encounter", "/billing-review", "/final-output"],
 };
